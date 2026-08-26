8e58ebe8d2e5aec5fc34ef622d3dac480635ad80
REM <esmaeilireza1994@gmail.com>
2026-08-27 01:30:58 +0330

    fix(build): add <cstdint> include and pin MinGW to gnu++14

--- src/sys/snap_platform.h
+++ src/sys/snap_platform.h
@@ -136,6 +136,7 @@
 // must be the same across the processor used 32/64 bit
 
 // *Use them* if you change/expand the code and avoid long, u_long and so on...
+#include <cstdint>
 
 typedef uint8_t    byte;
 typedef uint16_t   word;
